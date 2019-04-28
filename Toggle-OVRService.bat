0cf3d85936889808f5928f7de4fb1b5ce265d6c3
Seiya <45865749+SeiyaIskw@users.noreply.github.com>
2019-04-28 09:01:33 +0900

    Added message and /nobreak to timeout

--- Toggle-OVRService.bat
+++ Toggle-OVRService.bat
@@ -3,7 +3,8 @@
 
 net start "OVRService"
 if %errorlevel%==0 (
-	timeout /t 3
+	echo Please wait 3 seconds till boot Oculus Client
+	timeout /t 3 /NOBREAK
  	cd "C:\Program Files\Oculus\Support\oculus-client"
 	start OculusClient.exe
 	)
